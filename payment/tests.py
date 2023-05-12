a5827d2b9695548cb632aff5c7e9919d2841fd04
Adam Zbikowski <sc20aaz@leeds.ac.uk>
2023-05-12 23:06:12 +0100

    final code commit

--- payment/tests.py
+++ payment/tests.py
@@ -4,11 +4,12 @@ from .views import GetFormFields
 from .models import User, Transaction, Billing
 
 # Create your tests here.
-URL = 'https://sc20aaz.pythonanywhere.com'
-# URL = 'http://127.0.0.1:8000'
+# URL = 'http://sc20aaz.pythonanywhere.com'
+URL = 'http://127.0.0.1:8000'
 class TestGetFormFields(TestCase):
     def test_get_form_fields(self):
         response = requests.get(f'{URL}/payments/form')
+        print(response)
         data = response.json()
         self.assertEqual(data, {'fields' : {'Username':'String', 'Password':'String'}})
 
@@ -104,6 +105,16 @@ class TestMakeTransaction(TestCase):
         print(response.json())
         self.assertEqual(data.get('status'), 'success')
 
+         # Insuficient credentials
+        print('Insuficient funds')
+        payload = {'fields':{'Username':'adam','Password':'hashtest'},
+                         'transaction':
+                         {'amount':1_000_000_000,'currency':'GBP', 'recipientAccount':'Emirates', 'bookingID':21231231}}
+        response = requests.post(f'{URL}/payments/pay',json=payload)
+        data = response.json()
+        print(response.json())
+        self.assertEqual(data.get('status'), 'failed')
+
         print('Correct currency exchange')
         # test payment with currency exchange
         payload = {'fields':{'Username':'adam','Password':'hashtest'},
@@ -126,6 +137,40 @@ class TestMakeTransaction(TestCase):
 
         
 
-class TestRefundTransaction(TestCase):
-    def test_make_refund(sefl):
-        pass
+# class TestRefundTransaction(TestCase):
+    # def test_check_credentials(self):
+
+    #     print('Email as username')
+    #      # correct credentials usign email as username
+    #     payload = {'fields':{'Username':'azbik314@gmail.com','Password':'hashtest'},
+    #                      'transaction':
+    #                      {'TransactionID':30,'BookingID':66}}
+    #     response = requests.post(f'{URL}/payments/refund',json=payload)
+    #     data = response.json()
+    #     print(response.json())
+    #     self.assertEqual(data.get('status'), 'success')
+
+      
+    #     print('Username does not exist')
+    #     # username does not exist in database
+    #     payload = {'fields':{'Username':'notauser','Password':'wrongpassword'},
+    #                    'transaction':
+    #                      {'TransactionID':31,'BookingID':66}}
+    #     response = requests.post(f'{URL}/payments/pay',json=payload)
+    #     data = response.json()
+    #     print(data)
+    #     self.assertEqual(data.get('status'), 'failed')
+    #     self.assertEqual(data.get('error'), 'User does not exist')
+
+    #     print('Incorrect password')
+    #     # incorrect password
+    #     payload = {'fields':{'Username':'adam','Password':'wrongpassword'},
+    #                      'transaction':
+    #                      {'TransactionID':32,'BookingID':66}}
+    #     response = requests.post(f'{URL}/payments/pay',json=payload)
+    #     data = response.json()
+    #     print(data)
+    #     self.assertEqual(data.get('status'), 'failed')
+    #     self.assertEqual(data.get('error'), 'Incorrect password')
+    # def test_make_refund(sefl):
+    #     pass
